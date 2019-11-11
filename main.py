e55e85763908ca3245c4ad3a1908fefb7c6405c0
Bojan Joveski <bokicapk7@gmail.com>
2019-11-11 18:08:07 +0100

    Dodali prijavo prek piskotkov

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, make_response
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def prva_stran():
+    ime = request.cookies.get("ime")
+
+    return render_template("prva_stran.html", ime=ime)
+
+
+@app.route("/kontakt")
+def kontakt():
+    emaili = ["ime@example.com", "ime@gmail.com", "tretji@email.si"]
+    return render_template("kontakt.html", emaili=emaili)
+
+
+@app.route("/poslji-sporocilo", methods=["POST"])
+def poslji_sporocilo():
+    zadeva = request.form.get("zadeva")
+    sporocilo = request.form.get("sporocilo")
+
+    # Tukaj bi shranili te spremenljivki v bazo.
+
+    return render_template("sporocilo_poslano.html", zadeva=zadeva)
+
+@app.route("/prijava", methods=["POST"])
+def prijava():
+    ime = request.form.get("ime")
+
+    odgovor = make_response(redirect("/"))
+    odgovor.set_cookie("ime", ime)
+    return odgovor
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

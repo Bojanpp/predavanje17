a20ed97e7530a753b207a0c4ecffdfae97449819
Bojan Joveski <bokicapk7@gmail.com>
2019-11-14 18:02:24 +0100

    Naredili bazo z komentarji

--- modeli.py
+++ modeli.py
@@ -2,4 +2,9 @@ import os
 from sqla_wrapper import SQLAlchemy
 
 
-db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///podatkovna-baza.sqlite"))
+db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///podatkova-baza.sqlite"))
+
+class Komentar(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    avtor = db.Column(db.String)
+    vsebina = db.Column(db.String)

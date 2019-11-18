8dcae6a1697dccc56604b5ba9f4b41c0d2e32e15
Bojan Joveski <bokicapk7@gmail.com>
2019-11-18 18:19:15 +0100

    dodal Uporabnik

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+import uuid
 from flask import Flask, render_template, request, redirect, make_response
-from modeli import Komentar, db
+from modeli import Komentar, db, Uporabnik
 
 app = Flask(__name__)
 db.create_all()
@@ -7,7 +9,13 @@ db.create_all()
 
 @app.route("/")
 def prva_stran():
-    ime = request.cookies.get("ime")
+    sejna_vrednost = request.cookies.get("sejna_vrednost")
+
+    uporabnik = db.query(Uporabnik).filter_by(sejna_vrednost=sejna_vrednost).first()
+    if uporabnik:
+        ime = uporabnik.ime
+    else:
+        ime = None
 
     # Preberemo vse komentarje
     komentarji = db.query(Komentar).all()
@@ -34,8 +42,19 @@ def poslji_sporocilo():
 def prijava():
     ime = request.form.get("ime")
 
+    sejna_vrednost = str(uuid.uuid4())
+
+    uporabnik = db.query(Uporabnik).filter_by(ime=ime).first()
+    if not uporabnik:
+        uporabnik = Uporabnik(ime=ime, sejna_vrednost=sejna_vrednost)
+    else:
+        uporabnik.sejna_vrednost = sejna_vrednost
+
+    db.add(uporabnik)
+    db.commit()
+
     odgovor = make_response(redirect("/"))
-    odgovor.set_cookie("ime", ime)
+    odgovor.set_cookie("sejna_vrednost", sejna_vrednost)
     return odgovor
 
 
@@ -43,10 +62,13 @@ def prijava():
 def poslji_komentar():
     vsebina_komentarja = request.form.get("vsebina")
 
+    sejna_vrednost = request.cookies.get("sejna_vrednost")
+    uporabnik = db.query(Uporabnik).filter_by(sejna_vrednost=sejna_vrednost).first()
+
     #Tukaj se bo shranil komentar v podatkovno bazo
 
     komentar = Komentar(
-        avtor=request.cookies.get("ime"),
+        avtor=uporabnik.ime,
         vsebina=vsebina_komentarja
     )
     db.add(komentar)
@@ -55,6 +77,25 @@ def poslji_komentar():
 
     return redirect("/")
 
+@app.route("/skrito-stevilo")
+def skrito_stevilo():
+    odgovor=make_response(render_template("skrito_stevilo.html"))
+
+    if not request.cookies.get("SkritoStevilo"):
+        stevilo = str(random.randint(1, 20))
+        odgovor.set_cookie("SkritoStevilo", stevilo)
+
+    return odgovor
+
+@app.route("/poslji-skrito-stevilo", methods=["POST"])
+def poslji_skrito_stevilo():
+    skrito_stevilo = request.cookies.get("SkritoStevilo")
+    vpisano_stevilo = request.form.get("stevilo")
+
+    if skrito_stevilo == vpisano_stevilo:
+        return "PRAVILNO"
+    else:
+        return "NI PRAVILNO"
 
 if __name__ == '__main__':
     app.run(debug=True)

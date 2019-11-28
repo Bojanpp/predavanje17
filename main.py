26d97a75dc80b311f7fa27cf65a4a71a0e01283c
Bojan Joveski <bokicapk7@gmail.com>
2019-11-28 16:59:32 +0100

    Dodal blokiranje uporabnika

--- main.py
+++ main.py
@@ -53,6 +53,9 @@ def prijava():
     if not uporabnik:
         uporabnik = Uporabnik(ime=ime, email="", geslo=geslo, sejna_vrednost=sejna_vrednost)
     else:
+        if uporabnik.je_blokiran:
+            return "Uporabnik blokiran, prijava ni mogoca"
+
         if geslo == uporabnik.geslo:
             uporabnik.sejna_vrednost = sejna_vrednost
         else:
@@ -160,9 +163,21 @@ def uporabniki():
     return render_template("uporabniki.html", uporabniki=users)
 
 
-@app.route("/uporabniki/<uporabnik_id>")
+@app.route("/uporabniki/<uporabnik_id>", methods=["GET", "POST"])
 def prikaz_uporabnika(uporabnik_id):
     uporabnik = db.query(Uporabnik).filter_by(id=uporabnik_id).first()
+
+    if request.method == "POST":
+
+        blokiran = False
+        if request.form.get("je_blokiran") == "on":
+            blokiran = True
+
+        uporabnik.je_blokiran = blokiran
+
+        db.add(uporabnik)
+        db.commit()
+
     return render_template("prikaz_uporabnika.html", uporabnik=uporabnik)
 
 
